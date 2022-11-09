da57339e9614902d15cd2068cfedc2790cfcb6bf
W1002IntentData <gmlghd98@gmail.com>
2022-11-09 15:22:32 +0900

    스마트앱 프로그래밍 10주차 3차시

--- app/src/main/java/kr/ac/kumoh/s20170523/w1002stringlist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170523/w1002stringlist/MainActivity.kt
@@ -1,12 +1,20 @@
 package kr.ac.kumoh.s20170523.w1002stringlist
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kr.ac.kumoh.s20170523.w1002stringlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val songs = arrayOf(
+        "I like you", "Cold feet", "Free time", "Shape of you",
+        "I like you", "Cold feet", "Free time", "Shape of you",
+        "I like you", "Cold feet", "Free time", "Shape of you",
+        "I like you", "Cold feet", "Free time", "Shape of you",
+        "I like you", "Cold feet", "Free time", "Shape of you",
         "I like you", "Cold feet", "Free time", "Shape of you",
     )
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +22,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.list.
+        binding.list.adapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                songs)
+
+        binding.list.setOnItemClickListener { _, _, i, _ ->
+            val uri = Uri.parse("http://youtube.com/results?search_query=노래방+" + songs[i])
+            val youtube = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(youtube)
+        }
     }
 }
